018e20ade5f83fb4cc66e6c4cc0728a4d5169623
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-05-22 18:47:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from reconhecimento_facial import camera, face_thread, exibir_frame, janela
+from reconhecimento_facial import face_thread, exibir_frame, janela
+from openpyxl import load_workbook
 
+
+wbf = load_workbook('/home/vyral/Vídeos/Face_recognizer/Funcionarios.xlsx')
+wbc = load_workbook('/home/vyral/Vídeos/Face_recognizer/Convidados.xlsx')
+
+wsf = wbf.active
+wsc = wbc.active
+
+wsf.delete_rows(1, wsf.max_row)
+wsc.delete_rows(1, wsc.max_row)
+
+wbf.save('/home/vyral/Vídeos/Face_recognizer/Funcionarios.xlsx')
+wbc.save('/home/vyral/Vídeos/Face_recognizer/Convidados.xlsx')
 
 face_thread.start()
 exibir_frame()
 janela.mainloop()
+
+wbf.close()
+wbc.close()
